d1ab8081cb879cfc9323070d584f1cdd6b951223
GOLDER303 <szymonstepien@interia.pl>
2023-10-27 19:37:54 +0200

    Add triangle images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import triangle1 from "./img/triangle1.svg"
+
 function App() {
-    return <h1>Hello World!</h1>
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <img
+                src={triangle1}
+                alt="triangle"
+            />
+        </>
+    )
 }
 
 export default App
